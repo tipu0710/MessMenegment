5dcb5db3e2e34919b2dd88740b7ad3bafe527016
Tipu <tsultan0710@gmail.com>
2018-01-16 01:34:03 +0600

    Permission Check problem fixed Previous Table Data 0 in Add Member fixed

--- app/src/main/java/com/example/tsult/messmenegment/AddMember/AddFromPrevious.java
+++ app/src/main/java/com/example/tsult/messmenegment/AddMember/AddFromPrevious.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.tsult.messmenegment.Home.Main2Activity;
@@ -20,6 +22,7 @@ public class AddFromPrevious extends AppCompatActivity {
     private RecyclerView.Adapter adapter;
     private String previousMonth;
     private AddMemberDBOperation addMemberDBOperation;
+    private TextView showText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,13 +32,19 @@ public class AddFromPrevious extends AppCompatActivity {
         members = new ArrayList<>();
 
         addMemberDBOperation = new AddMemberDBOperation(this);
-        previousMonth = (addMemberDBOperation.getAllTables(MealInfo.getYear() +" - "+ MealInfo.getMonth())).get(0).getName();
+        if (addMemberDBOperation.getAllTables(MealInfo.getYear() +" - "+ MealInfo.getMonth()).size()>0){
+            previousMonth = (addMemberDBOperation.getAllTables(MealInfo.getYear() +" - "+ MealInfo.getMonth())).get(0).getName();
 
-        members = addMemberDBOperation.getMemberList(previousMonth);
-        previousList.setHasFixedSize(true);
-        previousList.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new ContactListAdapter(members, this, true);
-        previousList.setAdapter(adapter);
+            members = addMemberDBOperation.getMemberList(previousMonth);
+            previousList.setHasFixedSize(true);
+            previousList.setLayoutManager(new LinearLayoutManager(this));
+            adapter = new ContactListAdapter(members, this, true);
+            previousList.setAdapter(adapter);
+        }else {
+            showText = (TextView) findViewById(R.id.none_text);
+            showText.setVisibility(View.VISIBLE);
+        }
+
     }
 
     @Override
--- app/src/main/java/com/example/tsult/messmenegment/AddMember/ContactActivity.java
+++ app/src/main/java/com/example/tsult/messmenegment/AddMember/ContactActivity.java
@@ -1,20 +1,26 @@
 package com.example.tsult.messmenegment.AddMember;
 
 import android.Manifest;
+import android.annotation.TargetApi;
 import android.content.ContentResolver;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.provider.ContactsContract;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.tsult.messmenegment.Home.Main2Activity;
 import com.example.tsult.messmenegment.R;
@@ -27,15 +33,17 @@ public class ContactActivity extends AppCompatActivity {
     private RecyclerView contactListView;
     private RecyclerView.Adapter adapter;
     private static final int PERMISSION_REQUEST_CONTACT = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
         contactListView = (RecyclerView) findViewById(R.id.contact_list);
 
-        addMemberDetailInfos = new ArrayList<>();
-        getContactList();
 
+        addMemberDetailInfos = new ArrayList<>();
+
+        askForContactPermission();
         contactListView.setHasFixedSize(true);
         contactListView.setLayoutManager(new LinearLayoutManager(this));
         adapter = new ContactListAdapter(addMemberDetailInfos, this);
@@ -43,14 +51,12 @@ public class ContactActivity extends AppCompatActivity {
     }
 
     private void getContactList() {
-
-        showContact();
         ContentResolver contentResolver = getContentResolver();
         Cursor cursorAndroidContact;
-        cursorAndroidContact = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+        cursorAndroidContact = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, "UPPER(" + ContactsContract.Contacts.DISPLAY_NAME + ") ASC");
 
-        if (cursorAndroidContact.getCount() > 0){
-            while (cursorAndroidContact.moveToNext()){
+        if (cursorAndroidContact.getCount() > 0) {
+            while (cursorAndroidContact.moveToNext()) {
 
                 AddMemberDetailsInfo addMemberDetailsInfo = new AddMemberDetailsInfo();
 
@@ -59,16 +65,16 @@ public class ContactActivity extends AppCompatActivity {
 
                 addMemberDetailsInfo.setmName(contactDisplayName);
 
-                int hasPhoneNumber = Integer.parseInt( cursorAndroidContact.getString(cursorAndroidContact.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)));
+                int hasPhoneNumber = Integer.parseInt(cursorAndroidContact.getString(cursorAndroidContact.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)));
 
-                if (hasPhoneNumber > 0){
+                if (hasPhoneNumber > 0) {
                     Cursor phoneCursor = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
                             null,
                             ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
                             new String[]{contactID},
                             null);
 
-                    while (phoneCursor.moveToNext()){
+                    while (phoneCursor.moveToNext()) {
                         String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
                         addMemberDetailsInfo.setmPhone(phoneNumber);
                     }
@@ -90,19 +96,73 @@ public class ContactActivity extends AppCompatActivity {
         startActivity(new Intent(this, Main2Activity.class));
     }
 
-    private void showContact(){
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED){
-            requestContactPermission();
+    public void askForContactPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+
+                // Should we show an explanation?
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.READ_CONTACTS)) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Contacts access needed");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setMessage("Please confirm Contacts access to show which member you need to add");//TODO put real question
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @TargetApi(Build.VERSION_CODES.M)
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                            requestPermissions(
+                                    new String[]
+                                            {Manifest.permission.READ_CONTACTS}
+                                    , PERMISSION_REQUEST_CONTACT);
+                        }
+                    });
+                    builder.show();
+                    // Show an expanation to the user *asynchronously* -- don't block
+                    // this thread waiting for the user's response! After the user
+                    // sees the explanation, try again to request the permission.
+
+                } else {
+
+                    // No explanation needed, we can request the permission.
+
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.READ_CONTACTS},
+                            PERMISSION_REQUEST_CONTACT);
+
+                    // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                    // app-defined int constant. The callback method gets the
+                    // result of the request.
+
+                    getContactList();
+                }
+            } else {
+                getContactList();
+            }
+        } else {
+            getContactList();
         }
     }
 
-    private void requestContactPermission(){
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CONTACTS)){
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.READ_CONTACTS},
-                    PERMISSION_REQUEST_CONTACT);
-        }else {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, PERMISSION_REQUEST_CONTACT);
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CONTACT: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getContactList();
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+                    Toast.makeText(this, "No Permissions ", Toast.LENGTH_SHORT).show();
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
         }
     }
 }
